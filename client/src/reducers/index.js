691dd9fe56e4327dbc5a65d45d731891d2283165
Akansh8 <Akanshk550@gmail.com>
2021-03-30 19:10:01 +0530

    removed client folder

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { authReducer } from "./auth";
-// combine reducer
-const rootReducer = combineReducers({
-  user: authReducer,
-});
-
-export default rootReducer;
